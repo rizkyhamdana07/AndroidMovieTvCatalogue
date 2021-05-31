f25e9f1193ddd5bf548aed1369989b09513fd41d
rizkyhamdana07 <rizky.hamdana07@gmail.com>
2021-05-31 20:31:36 +0800

    DynamicNavigation

--- app/src/main/java/com/rizkyhamdana/movietvcatalogue/home/HomeActivity.kt
+++ app/src/main/java/com/rizkyhamdana/movietvcatalogue/home/HomeActivity.kt
@@ -3,7 +3,7 @@ package com.rizkyhamdana.movietvcatalogue.home
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.rizkyhamdana.movietvcatalogue.R
 import com.rizkyhamdana.movietvcatalogue.databinding.ActivityHomeBinding
@@ -20,7 +20,8 @@ class HomeActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         navView.setupWithNavController(navController)
     }
 }
